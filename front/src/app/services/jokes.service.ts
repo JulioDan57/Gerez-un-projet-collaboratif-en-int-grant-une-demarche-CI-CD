bba5c5e9194692c55508cd897a983af754b1f42b
JulioDan57 <daniel.gilcano@yahoo.com>
2026-03-05 17:03:26 +0100

    trigger CI pipelines

--- front/src/app/services/jokes.service.ts
+++ front/src/app/services/jokes.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Joke } from '../model/joke.model';
 
-// trigger CI
 
 @Injectable({
   providedIn: 'root'
